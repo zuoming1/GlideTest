8d7ef13256d3b20e0e56618d02ec70f560b7b272
w2296 <w2296635105@163.com>
2023-10-04 18:11:09 +0800

    initail commit

--- app/src/main/java/com/example/glidetest/MainActivity.java
+++ app/src/main/java/com/example/glidetest/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.glidetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory;
 
 public class MainActivity extends AppCompatActivity {
     ImageView IImageView;
@@ -23,9 +27,11 @@ public class MainActivity extends AppCompatActivity {
                 .fallback(R.drawable.fallback)//如果请求的url/model为null时的展示照片（如果没有设置则展示placeholder）
                 .override(100,100);//图片宽高
 
+
         Glide.with(this)
                 .load("http://39.103.216.115:8081/music/1.png")
                 .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade(3000))
                 .into(IImageView);
 
     }
